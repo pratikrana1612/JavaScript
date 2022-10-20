b0e102ee21d8c123b086687663f1ce547f449169
Rana Pratik <ranapratik1612@gmail.com>
2022-10-20 23:02:07 +0530

    advanced recursion

--- app.js
+++ app.js
@@ -5,27 +5,24 @@
 
 
 let multipler = 1.2;
-function createTaxCaluculator(tax)
-{
-    function calculateTax(amount)
-    {
+function createTaxCaluculator(tax) {
+    function calculateTax(amount) {
         console.log(multipler);
-        return amount*tax*multipler;
+        return amount * tax * multipler;
     }
     return calculateTax;
 }
 
-const calculateVatAmount = createTaxCaluculator(0.19); 
+const calculateVatAmount = createTaxCaluculator(0.19);
 const calculateIncomeTax = createTaxCaluculator(0.25);
 
-multipler=1.5;
+multipler = 1.5;
 console.log(calculateVatAmount(100));
 console.log(calculateIncomeTax(200));
 
 let userName = 'pratik';
-function greetUser()
-{
-    const name= 'Sohan';
+function greetUser() {
+    const name = 'Sohan';
     console.log('hi ' + name);
 }
 let name = 'Mohan';
@@ -35,4 +32,72 @@ greetUser();
 // a=20;b=23;
 // console.log(sum());
 
+function powerOf(x, n) {
+    // if(n===1)
+    // {
+    //     return x;
+    // }
+    // return x*powerOf(x,n-1);
+    return n === 1 ? x : x * powerOf(x, n - 1);
+}
 
+console.log(powerOf(2, 3));
+
+
+
+const myself = {
+    name:'Pratik',
+    friends:[
+        {
+            name:'Rohan',
+            friends:[
+                {
+                    name:'Varun',
+                    friends:[
+                        {
+                            name:'Deep'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name:'Julia',
+            friends:[
+                {
+                    name:'Varun'
+                }
+            ]
+        }
+    ]
+}
+
+// function printFrieds(person)
+// {
+//     for(const friend of person.friends)
+//     {
+//         for(const friend2 of friend.friends)
+//         {
+//             console.log(friend2);
+//         }
+//     }
+// }
+
+
+function printFrieds(person)
+{
+    const friendsStore = [];
+    if(!person.friends)
+    {
+        return [];
+    }
+    for(const friend of person.friends)
+    {
+        friendsStore.push(friend.name);
+        friendsStore.push(...printFrieds(friend));
+    }
+
+    return friendsStore;
+}
+
+console.log(printFrieds(myself));
