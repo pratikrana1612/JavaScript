87aa5810cbc4bf7b4a339c389f7498cd8d496b88
Rana Pratik <ranapratik1612@gmail.com>
2022-10-20 22:09:59 +0530

    closures

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
 // inpure function which is not the pure function 
 
 
-let multipler =
+let multipler = 1.2;
 function createTaxCaluculator(tax)
 {
     function calculateTax(amount)
     {
-        return amount*tax;
+        console.log(multipler);
+        return amount*tax*multipler;
     }
     return calculateTax;
 }
@@ -17,7 +18,21 @@ function createTaxCaluculator(tax)
 const calculateVatAmount = createTaxCaluculator(0.19); 
 const calculateIncomeTax = createTaxCaluculator(0.25);
 
-
+multipler=1.5;
 console.log(calculateVatAmount(100));
 console.log(calculateIncomeTax(200));
+
+let userName = 'pratik';
+function greetUser()
+{
+    const name= 'Sohan';
+    console.log('hi ' + name);
+}
+let name = 'Mohan';
+userName = 'Rohan';
+greetUser();
+
+// a=20;b=23;
+// console.log(sum());
+
 
